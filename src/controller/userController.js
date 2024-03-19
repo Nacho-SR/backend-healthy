290f5d325e00321ebdd72f4bf49302d73b092b68
Ignacio Salas R <146854109+Nacho-SR@users.noreply.github.com>
2024-03-19 09:24:12 -0600

    agregamos modelo e interfase

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,27 +1,23 @@
-const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
-const admin = require('./../config/firebase')
+const User = require('../models/User')
 
 const loginUser = async (req, res) => {
-  console.log('@@@ body =>', req.body)
   try {
     const { email, password } = req.body
 
     // Buscamos el usuario para verificar que existe el correo electronico
     // ahora con firebase-admin solo lo podemos poner asi
-    const userDoc = await admin.firestore().collection('users').doc(email).get()
+    const userDoc = await User.findByEmail(email)
 
     // Si no existe el usuario
-    if (!userDoc.exists) {
+    if (!userDoc) {
       return res.status(404).json({
         message: 'User not found'
       })
     }
 
-    const userData = userDoc.data()
-
     // Verificar si el password es correcto
-    const isValidPass = await bcrypt.compare(password, userData.password)
+    const isValidPass = await userDoc.verifyPassword(password)
 
     if (!isValidPass) {
       return res.status(401).json({
@@ -30,7 +26,7 @@ const loginUser = async (req, res) => {
     }
 
     // Generar el TOKEN
-    const token = jwt.sign({ email:userData.email }, process.env.SECRET, { expiresIn: '1h' })
+    const token = jwt.sign({ email:userDoc.email }, process.env.SECRET, { expiresIn: '1h' })
     res.status(200).json({ token })
   } catch (error) {
     res.status(500).json({
@@ -42,18 +38,18 @@ const loginUser = async (req, res) => {
 const registerUser = async (req, res) => {
   try {
     const { email, password } = req.body
+    const existingUser = await User.findByEmail(email)
+    if (existingUser) {
+      return res.status(400).json({
+        message: 'User already exists'
+      })
+    }
 
-    // hash password
-    const hashed = await bcrypt.hash(password, 10)
-
-    // Guardar en la BD
-    await admin.firestore().collection('users').doc(email).set({
-      email: email,
-      password: hashed
-    })
+    const newUser = await User.createUser(email, password)
 
     res.status(201).json({
-      message: 'User registered successfully'
+      message: 'User registered successfully',
+      user: newUser
     })
   } catch {
     res.status(500).json({
